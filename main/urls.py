a310b27e841eaed6873249fd76c202f98bbbe5b9
thanapat <thanapatspongpipat@gmail.com>
2023-03-11 22:38:42 +0700

    complete func upload file

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/file-upload', views.file_upload, name='upload'),
+    path('dropzone-files', views.dropzone_files, name="dropzone-files"),
 ]
