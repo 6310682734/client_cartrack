545ac098c3511f5204a0206e2d7fed2badff7dfb
thanapat <thanapatspongpipat@gmail.com>
2023-03-12 23:24:41 +0700

    add button cancle upload file

--- main/static/main/app.js
+++ main/static/main/app.js
@@ -1,12 +1,14 @@
 var Drop = Dropzone.options.DidDropzone = {
 
     autoProcessQueue: false, //stops from uploading files until user submits form
+    addRemoveLinks: true,
     paramName: "file", // The name that will be used to transfer the file
     maxFilesize: 5, // Maximum size of file that you will allow (MB)
     clickable: true, // This allows the dropzone to select images onclick
     acceptedFiles: '.jpg, .jpeg, .JPEG, .JPG, .png, .PNG, .mp4, .MP4', //accepted file types
     maxFiles: 10, //Maximum number of files/images in dropzone
     parallelUploads: 10,
+    dictRemoveFile: 'X',
     previewTemplate: '<div class="dz-preview dz-image-preview">'+
                         '<div class="dz-image">'+
                         '<img data-dz-thumbnail />'+
@@ -37,6 +39,7 @@ var Drop = Dropzone.options.DidDropzone = {
 
         myDropzone.on("complete", function(file) {
             myDropzone.removeFile(file);
+            
         });
     },
     success: function(file, json){
