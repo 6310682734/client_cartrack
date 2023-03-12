3ba7c5b21a32c74e1c06f873fd65ddefd8a5e747
thanapat <thanapatspongpipat@gmail.com>
2023-03-13 03:39:25 +0700

    add modal newtask and modify index

--- main/static/main/app.js
+++ main/static/main/app.js
@@ -48,3 +48,27 @@ var Drop = Dropzone.options.DidDropzone = {
         
     },
 }
+
+function showForm(selectEl) {
+  let selectedValue = selectEl.options[selectEl.selectedIndex].value;
+  let subForms = document.getElementsByClassName('form-newtask') 
+  for (let i = 0; i < subForms.length; i += 1) {
+    if (selectedValue === subForms[i].name) {
+      subForms[i].setAttribute('style', 'display:block')
+    } else {
+      subForms[i].setAttribute('style', 'display:none') 
+    }
+  }
+}
+
+function showFormIndex(selectEl) {
+  let selectedValue = selectEl.options[selectEl.selectedIndex].value;
+  let subForms = document.getElementsByClassName('form-index')
+  for (let i = 0; i < subForms.length; i += 1) {
+    if (selectedValue === subForms[i].name) {
+      subForms[i].setAttribute('style', 'display:block')
+    } else {
+      subForms[i].setAttribute('style', 'display:none') 
+    }
+  }
+}
