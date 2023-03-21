c80007bdf8aaec7592ed5229ba3506616dbe1030
6310682791 <6310682791@student.tu.ac.th>
2023-03-21 21:50:54 +0700

    create function register login logout

--- main/views.py
+++ main/views.py
@@ -1,32 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import json
 import os
 import fnmatch
 from .models import UserFiles
 from django.conf import settings
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='main:login')
 def index(req):
     return render(req, "main/index.html")
 
+@login_required(login_url='main:login')
 def dashboard(req):
     return render(req, "main/dashboard.html")
 
+@login_required(login_url='main:login')
 def edit(req):
     return render(req, "main/edit.html")
 
-def display_video(request,vid=None):
+
+def display_video(request, vid=None):
     if vid is None:
         return HttpResponse("No Video")
 
-    #Finding the name of video file with extension, use this if you have different extension of the videos    
+    # Finding the name of video file with extension, use this if you have different extension of the videos
     video_name = ""
     for fname in os.listdir(settings.MEDIA_ROOT):
-        if fnmatch.fnmatch(fname, vid+".*"): #using pattern to find the video file with given id and any extension
+        # using pattern to find the video file with given id and any extension
+        if fnmatch.fnmatch(fname, vid+".*"):
             video_name = fname
             break
-
 
     '''
         If you have all the videos of same extension e.g. mp4, then instead of above code, you can just use -
@@ -35,21 +43,52 @@ def display_video(request,vid=None):
 
     '''
 
-    #getting full url - 
+    # getting full url -
     video_url = settings.MEDIA_URL+video_name
 
-    return render(request, "edit.html", {"url":video_url})
+    return render(request, "edit.html", {"url": video_url})
 
+@login_required(login_url='main:login')
 def dropzone_files(request):
-	print(request.FILES.get('file'))
-	if request.method == "POST":
-		image = request.FILES.get('file')
-		img = UserFiles.objects.create(image = image)
-		
-		return HttpResponse({},content_type="application/json")
+    print(request.FILES.get('file'))
+    if request.method == "POST":
+        image = request.FILES.get('file')
+        img = UserFiles.objects.create(image=image)
 
-	return HttpResponse(
-		json.dumps({"result": result, "message": message}),
-		content_type="application/json"
-		)
+        return HttpResponse({}, content_type="application/json")
 
+    return HttpResponse(
+        json.dumps({"result": result, "message": message}),
+        content_type="application/json"
+    )
+
+
+#   Registrations
+def RegisterPage(request):
+    form = CreateUserForm
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main:login')
+
+    context = {"form": form}
+    return render(request, 'registration/register.html', context)
+
+
+def LoginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main:dashboard')
+    context = {}
+    return render(request, 'registration/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('main:login')
