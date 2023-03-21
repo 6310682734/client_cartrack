0b42d952c426cae753407cd5f0b595cb2b3e4b5c
6310682791 <6310682791@student.tu.ac.th>
2023-03-21 21:50:36 +0700

    add register,login,logout path

--- main/urls.py
+++ main/urls.py
@@ -8,9 +8,12 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('dashboard', views.dashboard, name='dashboard'),
-    path('edit', views.edit, name='edit'),
-    path('dropzone-files', views.dropzone_files, name="dropzone-files"),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('edit/', views.edit, name='edit'),
+    path('dropzone-files/', views.dropzone_files, name="dropzone-files"),
+    path('register/', views.RegisterPage, name='register'),
+    path('login/', views.LoginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout')
 ]
 
 if settings.DEBUG:
