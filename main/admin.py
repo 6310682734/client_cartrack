43158c299a1f467f3f752ade09d5e1afd0591f69
Adelerd <Flook.as2544@gmail.com>
2023-03-23 17:28:14 +0700

    add model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import JobTask
 
 # Register your models here.
+
+class JobTaskAdmin(admin.ModelAdmin):
+    list_display = ('locationName', 'latitude', 'longtitude', 'linkVideo', 'uid', 'jobId', 'status', 'createdAt', 'updatedAt')
+    list_filter = ('status',)
+    search_fields = ('locationName', 'uid')
+
+admin.site.register(JobTask, JobTaskAdmin)
